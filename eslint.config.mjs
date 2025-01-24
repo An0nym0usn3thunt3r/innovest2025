97b7f12560f7367dcce1cfad630f20dba94b5632
An0nym0us n3thunt3r <berajoy04@gmail.com>
2025-01-24 06:05:36 +0000

    eslint kyu hai aisa, config changes 	modified:   eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,19 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off', // You can change 'error' to 'warn' if you prefer
+  },
+  ignorePatterns: ['.next/*', 'node_modules/*'], // Add paths to ignore here
+};
+
 export default eslintConfig;
