405a4bbb7f0bcf581b990c86150296ec000123c9
An0nym0usn3thunt3r <139343538+An0nym0usn3thunt3r@users.noreply.github.com>
2025-01-25 03:42:26 +0530

    modified:   components/footer/footer.tsx

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -6,7 +6,7 @@ import { CopyrightCard } from "@/components/footer/copyright-card";
 
 export function Footer() {
   return (
-    <footer className="w-full bg-background py-12 pt-16 px-4 md:px-6">  
+    <footer className="w-full bg-background py-12 pt-48 px-4 md:px-6">  
       <div className="container mx-auto max-w-6xl">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           <SocialLinksCard />
